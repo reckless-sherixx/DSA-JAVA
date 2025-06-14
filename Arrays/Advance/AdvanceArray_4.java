2d0e85a6a87b45f5067244d7f415a70eac8a9589
reckless-sherixx <ezboi2312@gmail.com>
2025-06-15 02:47:11 +0530

    Arrays

---
+++ Arrays/Advance/AdvanceArray_4.java
@@ -0,0 +1,32 @@
+package Arrays.Advance;
+
+public class AdvanceArray_4 {
+    public static int trapping_Rainwater(int height[]) {
+        //Calculate left max boundary
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        //Calculate right max boundary
+        int rightMax[] = new int[height.length];
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0 ; 
+        for ( int i = 0 ; i<height.length ; i++){
+            //Calculate water level
+            int waterLevel = Math.min(leftMax[i] , rightMax[i]);
+            //Calculate trapped water
+            trappedWater += waterLevel - height[i];
+        } 
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2 , 0 , 6 , 3, 2,5};
+        System.out.println(trapping_Rainwater(height));
+    }
+}
