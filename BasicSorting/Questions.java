535af032be8239c34032582d6c53aa016caa6be0
reckless-sherixx <ezboi2312@gmail.com>
2025-06-17 13:39:50 +0530

    Sorting

---
+++ BasicSorting/Questions.java
@@ -0,0 +1,85 @@
+package BasicSorting;
+
+public class Questions {
+    public static void displayArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
+
+    // Bubble sort
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        displayArr(arr);
+    }
+
+    // Selection sort
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] < arr[j]) {
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+        displayArr(arr);
+    }
+
+    // Insertion sort
+    public static void insertionSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] < curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+        displayArr(arr);
+    }
+
+    // Count sort
+    public static void countSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+        displayArr(arr);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        // insertionSort(arr);
+        countSort(arr);
+    }
+}
