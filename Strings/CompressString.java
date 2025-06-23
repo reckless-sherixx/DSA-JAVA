57cfb1792de9f53856a83bc97dba96929d61ab4f
reckless-sherixx <ezboi2312@gmail.com>
2025-06-23 13:57:21 +0530

    Strings

---
+++ Strings/CompressString.java
@@ -0,0 +1,23 @@
+package Strings;
+
+public class CompressString {
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder("");
+        for(int i = 0 ; i<str.length(); i++){
+            int count = 1;
+            while(i<str.length() - 1 && str.charAt(i) == str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            sb.append(str.charAt(i));
+            if(count > 1 ) {
+                sb.append(count);
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        String str = "aaabbbcc";
+        System.out.println(compress(str));    
+    }
+}
