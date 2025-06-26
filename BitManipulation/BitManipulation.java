d248607bbd96663d66d53cad8215769fade7b517
reckless-sherixx <ezboi2312@gmail.com>
2025-06-27 00:23:54 +0530

    BitManipulation

--- BitManipulation/BitManipulation.java
+++ BitManipulation/BitManipulation.java
@@ -2,6 +2,13 @@ package BitManipulation;
 
 public class BitManipulation {
     public static void main(String[] args) {
+        //AND
         System.out.println((5 & 6));
+        //OR
+        System.out.println((5 | 6));
+        //XOR
+        System.out.println((5 ^ 6));
+        //1's complement
+        System.out.println((~5));
     }
 }
---
+++ BitManipulation/LeftAndRightShift.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class LeftAndRightShift {
+    public static void main(String[] args) {
+        //Left Shift
+        System.out.println((5 << 2));
+        //Right Shift
+        System.out.println((5 >> 2));
+    }
+}
