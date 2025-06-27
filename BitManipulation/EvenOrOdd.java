108e22c919b0e7ee24386cef364e029a9511fea6
reckless-sherixx <ezboi2312@gmail.com>
2025-06-27 23:59:55 +0530

    BitManipulation

---
+++ BitManipulation/EvenOrOdd.java
@@ -0,0 +1,15 @@
+package BitManipulation;
+
+public class EvenOrOdd {
+    public static void checkEvenOdd(int n){
+        int bitMask = 1;
+        if((n & bitMask )== 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+    public static void main(String[] args){
+        checkEvenOdd(7);
+    }
+}
