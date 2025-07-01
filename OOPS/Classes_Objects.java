f94bc97098f80ec506ae7c632ac94a9d4f625a4b
reckless-sherixx <ezboi2312@gmail.com>
2025-07-02 03:42:53 +0530

    OOPS

---
+++ OOPS/Classes_Objects.java
@@ -0,0 +1,22 @@
+package OOPS;
+
+public class Classes_Objects {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("Blue");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+    }
+}
+
+class Pen { 
+    String color ; 
+    int tip ;
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
