0f36d1f3c2c1a0f1e304b24cc1de92b24891c819
reckless-sherixx <ezboi2312@gmail.com>
2025-07-02 21:09:00 +0530

    ProblemSolving

---
+++ ProblemSolving/LeetCode/Strings/StringCompression.java
@@ -0,0 +1,26 @@
+package ProblemSolving.LeetCode.Strings;
+
+public class StringCompression {
+    class Solution {
+    public int compress(char[] chars) {
+       int i = 0 ; 
+       int index = 0;
+       while(i < chars.length){
+            int count = 0 ;
+            char currChar = chars[i];
+            while(i<chars.length && chars[i] == currChar){
+                count ++ ;
+                i++;
+            }
+            chars[index++]= currChar;    
+
+            if(count > 1){
+                for(char c : Integer.toString(count).toCharArray()){
+                    chars[index++] = c;
+                }
+            }
+       }
+       return index;
+    }
+} 
+}
