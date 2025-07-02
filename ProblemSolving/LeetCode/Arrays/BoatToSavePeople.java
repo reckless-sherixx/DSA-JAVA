3eb5c25a9dd9bdcaf81a4b134b4a09b9934388a9
reckless-sherixx <ezboi2312@gmail.com>
2025-07-02 21:25:47 +0530

    ProblemSolving

--- ProblemSolving/LeetCode/Arrays/BoatToSavePeople.java
+++ ProblemSolving/LeetCode/Arrays/BoatToSavePeople.java
@@ -3,16 +3,16 @@ package ProblemSolving.LeetCode.Arrays;
 import java.util.Arrays;
 
 public class BoatToSavePeople {
-    public int numRescueBoats(int[] people, int limit) { 
+    public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
-        int i = 0 , j = people.length-1;
-        int boat = 0 ;
-        while(i <= j){
-            if(people[i] + people[j] <= limit){
+        int i = 0, j = people.length - 1;
+        int boat = 0;
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
                 i++;
             }
             j--;
-            boat ++;
+            boat++;
         }
         return boat;
     }
